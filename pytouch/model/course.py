4f1e8c67adaa14a0683f589a0f39faddaf4eac66
Moritz Nisblé <moritz.nisble@gmx.de>
2016-08-17 11:54:50 +0200

    Enhanced Course model.

--- pytouch/model/course.py
+++ pytouch/model/course.py
@@ -12,18 +12,21 @@ class Lesson(Base):
 
     uuid = Column('pkLessonUuid', String, primary_key=True, default=lambda: str(uuid.uuid4()))
     title = Column('cLessonTitle', String, nullable=False)
-    newChars = Column('cNewChars', String)
+    new_chars = Column('cNewChars', String)
     builtin = Column('cLessonBuiltin', Boolean, default=False)
     text = Column('cText', String)
     course = relationship('LessonList', backref=backref('lesson'), cascade="all, delete-orphan")
+
+    def __repr__(self):
+        return '{self.uuid} -- builtin: {self.builtin!s:>5} -- {self.title}'.format(self=self)
 
 
 class LessonList(Base):
     __tablename__ = 'tblLessonList'
 
     id = Column('pkLessonListId', Integer, primary_key=True, autoincrement=True)
-    courseUuid = Column('fkCourseUuid', String, ForeignKey('tblCourse.pkCourseUuid', onupdate='CASCADE', ondelete='CASCADE'))
-    lessonUuid = Column('fkLessonUuid', String, ForeignKey('tblLesson.pkLessonUuid', onupdate='CASCADE', ondelete='CASCADE'))
+    course_uuid = Column('fkCourseUuid', String, ForeignKey('tblCourse.pkCourseUuid', onupdate='CASCADE', ondelete='CASCADE'))
+    lesson_uuid = Column('fkLessonUuid', String, ForeignKey('tblLesson.pkLessonUuid', onupdate='CASCADE', ondelete='CASCADE'))
     position = Column('position', Integer)
 
     def __init__(self, lesson=None, **kw):
@@ -39,5 +42,13 @@ class Course(Base):
     title = Column('cCourseTitle', String, nullable=False)
     description = Column('cDescription', String)
     builtin = Column('cCourseBuiltin', Boolean, default=False)
+    keyboard_layout = Column('cKbLayout', String)
     _lessons = relationship(LessonList, backref=backref('course'), cascade="all, delete-orphan", order_by=LessonList.position, collection_class=ordering_list('position'))
     lessons = association_proxy('_lessons', 'lesson')
+
+    def __repr__(self):
+        return '{self.uuid} -- builtin: {self.builtin!s:>5} -- lesson count: {lessons:2} -- {self.title}'.format(self=self, lessons=len(self.lessons))
+
+    @staticmethod
+    def find_all(session):
+        return session.query(Course)

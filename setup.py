e905667e1cfbdb79de4e5e76dc743b7e31e6f57a
Moritz Nisblé <moritz.nisble@gmx.de>
2016-08-17 11:49:08 +0200

    Added course files.

--- setup.py
+++ setup.py
@@ -4,13 +4,29 @@ setup(
     name='PyTouch',
     version='0.1.0',
     packages=find_packages(exclude=['tests', 'tools']),
+    # package_dir={'': 'pytouch'},
+    # scripts=['pytouch/main.py'],
     url='https://github.com/campact/kb-user',
     license='MIT License',
     author='Moritz Nisblé',
     author_email='moritz.nisble@gmx.de',
     description='',
+    package_data={
+        'pytouch.resources.courses': ['*.xsd', '*.xml'],
+    },
     # Bootstrap nose to be able to replace setuptools test command by nosetests
     setup_requires=['nose>=1.0'],
-    install_requires=['SQLAlchemy'],
+    install_requires=[
+        'SQLAlchemy',
+        'lxml'
+    ],
     tests_require=['coverage'],
+    entry_points={
+        'console_scripts': [
+            'pytouch = pytouch.main:run',
+        ],
+        'gui_scripts': [
+            'pytouch-gui = pytouch.main:run',
+        ]
+    }
 )

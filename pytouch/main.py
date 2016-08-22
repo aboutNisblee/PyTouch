138c8a3567ed7011631d856096a9cc97d2312cdc
Moritz Nisblé <moritz.nisble@gmx.de>
2016-08-22 21:57:49 +0200

    Logging and verbosity control.

--- pytouch/main.py
+++ pytouch/main.py
@@ -1,3 +1,4 @@
+import logging
 import argparse
 
 import sys
@@ -6,27 +7,33 @@ from pytouch.model import get_engine, Session, reset_db
 from pytouch.gui.tk import window
 
 
-def init_db(arg_map):
-    engine = get_engine({'sqlalchemy.url': arg_map['database']})
+def init_db(args):
+    db_uri = getattr(args, 'database')
+    logging.debug('Database URI: {}'.format(db_uri))
+    engine = get_engine({'sqlalchemy.url': db_uri})
     Session.configure(bind=engine)
 
 
-def reset_database(arg_map):
+def reset_database(args):
     from pytouch.service import CourseService
 
-    init_db(arg_map)
+    init_db(args)
     reset_db()
     CourseService.init_courses()
 
 
-def run(arg_map=None):
-    init_db(arg_map)
+def run(args=None):
+    init_db(args)
     window.MainWindow().show()
 
 
 def manage():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
+
+    verbosity_group = parser.add_mutually_exclusive_group()
+    verbosity_group.add_argument('--verbose', '-v', action='count', default=0, help='Increase verbosity')
+    verbosity_group.add_argument('--quiet', '-q', action='store_true', help='Reduce verbosity')
 
     # FIXME: Database path incorrect! Depends on installation path!
     parser.add_argument('--database', type=str, default='sqlite:///tests.sqlite', help='Change the default database')
@@ -36,11 +43,12 @@ def manage():
     parser_setup.set_defaults(fun=reset_database)
 
     args = parser.parse_args()
-    if not args.fun:
-        parser.print_help()
-        sys.exit()
 
-    arg_map = vars(args).copy()
-    arg_map.pop('fun')
+    lut_verbosity = {0: logging.WARNING, 1: logging.INFO, 2: logging.DEBUG}
+    level = logging.FATAL if getattr(args, 'quiet') else lut_verbosity.get(getattr(args, 'verbose', 0), logging.DEBUG)
+    # logging.basicConfig(level=level, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.basicConfig(level=level, format='%(name)s: %(levelname)s: %(message)s')
 
-    args.fun(arg_map)
+    logging.info('Configuration:\n\t{}'.format('\n\t'.join(['{}: {}'.format(k, getattr(v, '__name__', v)) for k, v in sorted(args.__dict__.items())])))
+
+    args.fun(args)
